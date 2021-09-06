ef29110a43511849f2cbe27d67b221fb91af9664
Saurabh Hirani <saurabh.hirani@gmail.com>
2021-09-06 19:12:18 +0530

    parallel: derive cpu from lscpu

--- aws-profile-run-parallel.sh
+++ aws-profile-run-parallel.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-grep '\[' ~/.aws/credentials | sed 's/\[//g;s/\]//g;' | parallel -P 8 "echo {} && mkdir -p /var/tmp/aws/{} && aws $2 --profile {} > /var/tmp/aws/{}/$1.json 2>&1"
+# inspired by https://twitter.com/arjunmahishi/status/1434781043358265348
+\grep '\[' ~/.aws/credentials | sed 's/\[//g;s/\]//g;' | parallel -P $(lscpu | \grep -P '^CPU\(s\)' | awk '{ print $2 }') "echo {} && mkdir -p /var/tmp/aws/{} && aws $2 --profile {} > /var/tmp/aws/{}/$1.json 2>&1"
