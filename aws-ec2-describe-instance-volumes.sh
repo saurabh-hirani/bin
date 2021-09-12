def9bcba1aa4e0e7694c1c9a8b02c33d13710088
Saurabh Hirani <saurabh.hirani@gmail.com>
2021-09-12 19:45:34 +0530

    add instance id to differentiate dup name instances

--- aws-ec2-describe-instance-volumes.sh
+++ aws-ec2-describe-instance-volumes.sh
@@ -4,11 +4,12 @@
 # $0 # without filters
 # $0 "Name=tag:eks:nodegroup-name,Values=nodegroup" "Name=instance-state-name,Values=running" # with filters
 
-aws ec2 describe-instances --filters $* | jq -r '.Reservations[].Instances[] | [((.Tags[] | select(.Key=="Name") | .Value) // .InstanceId), .BlockDeviceMappings[].Ebs.VolumeId] | @csv' | while read line; do
-  name=$(echo "$line" | cut -f1 -d ',' | tr -d '"')
-  volume_ids=$(echo "$line" | cut -f2- -d ',' | tr  ',' ' '| tr -d '"')
+aws ec2 describe-instances --filters $* | jq -r '.Reservations[].Instances[] | [.InstanceId, ((.Tags[] | select(.Key=="Name") | .Value) // "empty_name"), .BlockDeviceMappings[].Ebs.VolumeId] | @csv' | while read line; do
+  instance_id=$(echo "$line" | cut -f1 -d ',' | tr -d '"')
+  name=$(echo "$line" | cut -f2 -d ',' | tr -d '"')
+  volume_ids=$(echo "$line" | cut -f3- -d ',' | tr  ',' ' '| tr -d '"')
   for volume_id in $(echo "$volume_ids" | cut -f1- -d' '); do
-    echo -n "$name,$volume_id,"
+    echo -n "$instance_id,$name,$volume_id,"
     aws ec2 describe-volumes --volume-ids "$volume_id" | jq -r '.Volumes[] | [.Attachments[].Device, .Size]| @csv' | tr -d '"'
   done
 done
