030c856b164ea151706d59f33340965dc2548b8d
Saurabh Hirani <saurabh.hirani@gmail.com>
2022-08-02 11:34:19 +0530

    updating termgraph wrapper

--- termgraph-it.sh
+++ termgraph-it.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+### Sample usage
+# for i in $(seq 1 10); do shuf -i 1-10 -n 1; done | termgraph-it.sh
+### Sample usage
+
 sort -nr | uniq -c | sort -nr -k1 | awk '{ print $2, $1 }' | termgraph
