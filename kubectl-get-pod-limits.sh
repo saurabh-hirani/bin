3660c0436cd456968f0d2cf961e458dec1af7ca7
Saurabh Hirani <saurabh.hirani@gmail.com>
2022-09-16 15:44:03 +0530

    kubectl: update get pod limits grep expr

--- kubectl-get-pod-limits.sh
+++ kubectl-get-pod-limits.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-kubectl get pods --all-namespaces 2>/dev/null| grep Running | awk '{ print $1, $2 }' | tr -s ' ' | while read line; do 
+kubectl get pods --all-namespaces 2>/dev/null| grep Running | awk '{ print $1, $2 }' | tr -s ' ' | while read line; do
   ns=$(echo $line | awk '{ print $1 }')
   pod=$(echo $line | awk '{ print $2 }')
-  limits=$(kubectl describe pod $pod -n $ns 2>/dev/null| grep -A2 ' Limits| Requests' | yaml2json | jq -c)
-  echo "$ns: $pod: $limits"
+  limits=$(kubectl describe pod "$pod" -n "$ns" 2>/dev/null | \grep -A10 ' Limits' | \grep -P 'cpu:|memory:' | head -2 | tr -d ' ' | tr '\n' ',')
+  [[ -z "$limits" ]] && limits="na"
+  requests=$(kubectl describe pod "$pod" -n "$ns" 2>/dev/null | \grep -A10 ' Requests' | \grep -P 'cpu:|memory:' | head -2 | tr -d ' ' | tr '\n' ',')
+  [[ -z "$requests" ]] && requests="na"
+  echo "ns=$ns pod=$pod limits=$limits requests=$requests"
 done
