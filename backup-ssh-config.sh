e072b6c76d2d0e4baeed9c532c1295476d03ede3
Saurabh Hirani <saurabh.hirani@gmail.com>
2022-09-26 19:09:33 +0530

    backup ssh config script

---
+++ backup-ssh-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Very basic script to backup ssh config because I nuked mine by mistake.
+
+curr_hour=$(date +%H)
+
+backup_dir="$HOME/backups/ssh/$curr_hour"
+
+if [[ -d "$backup_dir" ]]; then
+  # Avoid using $backup_dir var to be paranoid because rm -rf
+  rm -rf "$HOME/backups/ssh/$curr_hour"
+fi
+
+mkdir -p "$backup_dir"
+
+cp -rpv ~/.ssh/ "$backup_dir"
