9c3fea0034cf5a5b7ec4a6a4e618cf9aa0b5ffee
Saurabh Hirani <saurabh.hirani@gmail.com>
2023-06-01 22:05:50 +0530

    clean anybar

---
+++ clean-anybar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+osascript -e 'Tell application "AnyBar" to set title to ""'
+osascript -e 'Tell application "AnyBar_copy" to set title to ""'
+osascript -e 'Tell application "AnyBar" to set image name to "black"'
+osascript -e 'Tell application "AnyBar" to set image name to "black"'
+
