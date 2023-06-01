f7168f0d42cf6144018c266bdaafe3f8baf65dd8
Saurabh Hirani <saurabh.hirani@gmail.com>
2023-06-01 22:05:35 +0530

    minor change

--- split-track-time.sh
+++ split-track-time.sh
@@ -8,7 +8,6 @@ duration_mins="$1"
 one_fourth_duration=$(echo "scale=2;$duration_mins/4" | bc)
 two_fourth_duration="$(echo "scale=2;$duration_mins/3" | bc)"
 three_fourth_duration=$(echo "scale=2;$duration_mins/2" | bc)
-remaining_time=$duration_mins
 status_file="/tmp/timer-status.txt"
 success_count="0"
 
@@ -17,14 +16,18 @@ osascript -e 'Tell application "AnyBar_copy" to set title to ""'
 
 update_remaining_minutes() {
   local_duration_mins="$1"
-  for i in $(seq 0 $duration_mins); do
+  for i in $(seq 0 "$local_duration_mins"); do
     remaining_time=$(echo "$local_duration_mins - $i" | bc)
-    osascript -e 'Tell application "AnyBar_copy" to set title to "remaining_time='$remaining_time/$local_duration_mins' mins"'
+    osascript -e 'Tell application "AnyBar_copy" to set title to "session_remaining_time='$remaining_time/$local_duration_mins' mins"'
+    if [[ $remaining_time -eq 0 ]]; then
+      echo "breaking"
+      break
+    fi
     ((i++))
     sleep 60
   done
 }
-update_remaining_minutes $duration_mins &
+update_remaining_minutes "$one_fourth_duration" &
 
 osascript -e 'Tell application "System Events" to display dialog "Starting timer - do what needs to be done - you have '$duration_mins' minutes" with title "Starting"'
 osascript -e 'Tell application "AnyBar" to set image name to "cyan"'
@@ -37,19 +40,17 @@ timer "$one_fourth_duration" && osascript -e 'Tell application "System Events" t
 if grep -q OK < $status_file; then
   success_count="1"
 fi
-# remaining_time=$(echo "$duration_mins - $one_fourth_duration" | bc)
 osascript -e 'Tell application "AnyBar" to set title to "timer_status=1/4 score='$success_count/4'"'
 osascript -e 'Tell application "AnyBar" to set image name to "yellow"'
-# osascript -e 'Tell application "AnyBar_copy" to set title to "remaining_time='$remaining_time' mins"'
+update_remaining_minutes "$one_fourth_duration" &
 
 timer "$one_fourth_duration" && osascript -e 'Tell application "System Events" to display dialog "'"$two_fourth_duration/$duration_mins mins completed. score=$success_count/4"'" with title "2/4 time over"' > $status_file
 if grep -q OK < $status_file; then
   success_count=$((success_count+1))
 fi
-# remaining_time=$(echo "$duration_mins - $two_fourth_duration" | bc)
 osascript -e 'Tell application "AnyBar" to set title to "timer_status=2/4 score='$success_count/4'"'
 osascript -e 'Tell application "AnyBar" to set image name to "yellow"'
-# osascript -e 'Tell application "AnyBar_copy" to set title to "remaining_time='$remaining_time' mins"'
+update_remaining_minutes "$one_fourth_duration" &
 
 
 if [[ $success_count -gt 1 ]]; then
@@ -60,9 +61,14 @@ timer "$one_fourth_duration" && osascript -e 'Tell application "System Events" t
 if grep -q OK < $status_file; then
   success_count=$((success_count+1))
 fi
-# remaining_time=$(echo "$duration_mins - $three_fourth_duration" | bc)
 osascript -e 'Tell application "AnyBar" to set title to "timer_status=3/4 score='$success_count/4'"'
-# osascript -e 'Tell application "AnyBar_copy" to set title to "remaining_time='$remaining_time' mins"'
+update_remaining_minutes "$one_fourth_duration" &
+
+timer "$one_fourth_duration" && osascript -e 'Tell application "System Events" to display dialog "'"$duration_mins/$duration_mins mins completed - were you focused?"'" with title "Timer completed"' > $status_file
+
+if grep -q OK < $status_file; then
+  success_count=$((success_count+1))
+fi
 
 if [[ $success_count -gt 2 ]]; then
   osascript -e 'Tell application "AnyBar" to set image name to "green"'
@@ -70,13 +76,8 @@ else
   osascript -e 'Tell application "AnyBar" to set image name to "yellow"'
 fi
 
-timer "$one_fourth_duration" && osascript -e 'Tell application "System Events" to display dialog "'"$duration_mins/$duration_mins mins completed - were you focused?"'" with title "Timer completed"' > $status_file
-
-if grep -q OK < $status_file; then
-  success_count=$((success_count+1))
-fi
 osascript -e 'Tell application "AnyBar" to set title to "timer_status=4/4 score='$success_count/4'"'
-osascript -e 'Tell application "AnyBar_copy" to set title to "remaining_time=0 mins"'
+update_remaining_minutes "$one_fourth_duration" &
 
 if [[ $success_count -lt 2 ]]; then
   osascript -e 'Tell application "AnyBar" to set image name to "red"'
