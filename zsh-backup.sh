69f5a330d289d28a540b5540c8594abe00277f7e
Saurabh Hirani <saurabh.hirani@gmail.com>
2025-12-15 21:42:45 +0530

    zsh: prevent zsh history full from losing data

--- zsh-backup.sh
+++ zsh-backup.sh
@@ -14,10 +14,16 @@ cp "$HOME/.zsh_history" "$BACKUP_DIR/.zsh_history/" 2>/dev/null
 # Create full history - properly combine preserving multiline commands
 # from https://david-kerwick.github.io/2017-01-04-combining-zsh-history-files/
 MARKER="MULTILINE_$(date +"%s")"
-LC_ALL=C cat "$BACKUP_DIR"/.zsh_history/.zsh_history.* 2>/dev/null | \
+if [[ -f "$BACKUP_DIR/.zsh_history.full" ]]; then
+  FULL_FILE="$BACKUP_DIR/.zsh_history.full"
+else
+  FULL_FILE=""
+fi
+LC_ALL=C cat $FULL_FILE "$BACKUP_DIR"/.zsh_history/.zsh_history.* 2>/dev/null | \
   LC_ALL=C awk -v marker="$MARKER" '{if (sub(/\\$/,marker)) printf "%s", $0; else print $0}' | \
   LC_ALL=C sort -u | \
-  LC_ALL=C awk -v marker="$MARKER" '{gsub(marker,"\\\n"); print $0}' > "$BACKUP_DIR/.zsh_history.full"
+  LC_ALL=C awk -v marker="$MARKER" '{gsub(marker,"\\\n"); print $0}' > "$BACKUP_DIR/.zsh_history.full.tmp" && \
+  mv "$BACKUP_DIR/.zsh_history.full.tmp" "$BACKUP_DIR/.zsh_history.full"
 
 # Remove old files
 find "$BACKUP_DIR/.zshrc" -name ".zshrc.*" -mtime +30 -delete 2>/dev/null
